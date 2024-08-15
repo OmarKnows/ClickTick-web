d033094fe0fc2991f73468a2b3417746ba5c2d74
OmarKnows <omarknows98@gmail.com>
2024-08-15 13:53:08 +0300

    router setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ROUTES } from './constants/routes';
+import { LoginComponent } from './modules/login/login.component';
+import { ProductsComponent } from './modules/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ROUTES.LOGIN,
+    component: LoginComponent,
+  },
+  {
+    path: ROUTES.PRODUCTS,
+    component: ProductsComponent,
+  },
+];
