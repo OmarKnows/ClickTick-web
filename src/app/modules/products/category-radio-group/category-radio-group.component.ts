aa5b2b8545eff1e7ccc79f9b1c47dee7d3c7ec67
OmarKnows <omarknows98@gmail.com>
2024-08-15 16:30:39 +0300

    created category radio group

---
+++ src/app/modules/products/category-radio-group/category-radio-group.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-radio-group',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-radio-group.component.html',
+  styleUrl: './category-radio-group.component.scss'
+})
+export class CategoryRadioGroupComponent {
+
+}
