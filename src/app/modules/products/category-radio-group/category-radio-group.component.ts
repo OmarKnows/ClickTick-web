860c992b0c90f123fbba5f7f53ad380391c5c12d
omarknows <omarknows98@gmail.com>
2024-08-16 11:35:32 +0300

    finished category radio group ui

--- src/app/modules/products/category-radio-group/category-radio-group.component.ts
+++ src/app/modules/products/category-radio-group/category-radio-group.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { categories } from './tempCategories';
 
 @Component({
   selector: 'app-category-radio-group',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './category-radio-group.component.html',
-  styleUrl: './category-radio-group.component.scss'
+  styleUrl: './category-radio-group.component.scss',
 })
 export class CategoryRadioGroupComponent {
-
+  tempCategories = categories;
 }
