c7f0a8c2d43fd5c173cb87ed6dbd97bd6d54bcc8
omarknows <omarknows98@gmail.com>
2024-08-16 14:55:14 +0300

    added local storage and api constants

---
+++ src/app/constants/api.ts
@@ -0,0 +1,6 @@
+export enum EApi {
+  AUTH = '/auth',
+  PRODUCTS = '/products',
+}
+
+export const BASE_URL = 'https://dummyjson.com/';
---
+++ src/app/constants/local-storage.ts
@@ -0,0 +1,4 @@
+export enum ELocalStorage {
+  ACCESS_TOKEN = 'access_token',
+  REFRESH_TOKEN = 'refresh_token',
+}
