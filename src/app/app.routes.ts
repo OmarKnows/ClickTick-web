8d094c84e31bb8dc6de5146fae30be97a3da25ef
omarknows <omarknows98@gmail.com>
2024-08-16 20:03:11 +0300

    added fallback routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ERoutes } from './constants/routes';
 import { LoginComponent } from './modules/login/login.component';
 import { ProductsComponent } from './modules/products/products.component';
+import { AuthGuard } from './guards/auth.guards';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,8 @@ export const routes: Routes = [
   {
     path: ERoutes.PRODUCTS,
     component: ProductsComponent,
+    canActivate: [AuthGuard],
   },
+  { path: '', redirectTo: ERoutes.LOGIN, pathMatch: 'full' },
+  { path: '**', redirectTo: ERoutes.LOGIN },
 ];
