d87ba501f2b60c8784f14a235761fb5a6145268b
omarknows <omarknows98@gmail.com>
2024-08-16 22:02:49 +0300

    creating caching service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,27 +4,34 @@ import { catchError, tap, throwError } from 'rxjs';
 import { IProduct } from '../common/models/product.model';
 import { IPaginationParams, IResponse } from '../common/types';
 import { EApi } from '../constants/api';
+import { CachingService } from './caching.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
   private httpClient = inject(HttpClient);
+  private cachingService = inject(CachingService);
 
   listProducts(params?: IPaginationParams) {
     let skip = 0;
+
     if (params?.page) {
       params.page > 1 ? (skip = (params.page - 1) * 10) : (skip = 0);
     }
-    return this.httpClient
-      .get<IResponse<IProduct, 'products'>>(
-        EApi.PRODUCTS + `${skip > 1 ? `?skip=${skip}` : ''}`
-      )
-      .pipe(
+
+    const url = EApi.PRODUCTS + `${skip > 1 ? `?skip=${skip}` : ''}`;
+    const cachedResponse = this.cachingService.get(url);
+
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+      return this.httpClient.get<IResponse<IProduct, 'products'>>(url).pipe(
+        tap((response) => this.cachingService.set(url, response)),
         catchError((error: any) => {
-          console.log(error);
-          return throwError(() => new Error('An error occurred'));
+          return throwError(() => new Error(error.message));
         })
       );
+    }
   }
 }
