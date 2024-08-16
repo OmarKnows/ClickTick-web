01b2d13c337d7666523e2103b6907cf6544a759d
omarknows <omarknows98@gmail.com>
2024-08-16 14:55:55 +0300

    added httpclient and store provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
+import { ApiInterceptor } from './interceptors/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([ApiInterceptor])),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore(),
+  ],
 };
