af35d08d4874f5f2dcd9036ef6f0dc92ff3f4712
omarknows <omarknows98@gmail.com>
2024-08-16 14:35:17 +0300

    finished paginator ui

--- src/app/common/components/paginator/paginator.component.ts
+++ src/app/common/components/paginator/paginator.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './paginator.component.html',
-  styleUrl: './paginator.component.scss'
+  styleUrl: './paginator.component.scss',
 })
 export class PaginatorComponent {
+  @Input() totalPages: number = 0;
+  @Output() pageChange = new EventEmitter<number>();
 
+  currentPage: number = 1;
+  visiblePages: number[] = [];
+
+  ngOnInit() {
+    this.updateVisiblePages();
+  }
+
+  private updateVisiblePages() {
+    const maxVisiblePages = 5; // Adjust as needed
+    const startPage = Math.max(
+      1,
+      this.currentPage - Math.floor(maxVisiblePages / 2)
+    );
+    const endPage = Math.min(this.totalPages, startPage + maxVisiblePages - 1);
+
+    this.visiblePages = [];
+    for (let page = startPage; page <= endPage; page++) {
+      this.visiblePages.push(page);
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updateVisiblePages();
+      this.pageChange.emit(this.currentPage);
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateVisiblePages();
+      this.pageChange.emit(this.currentPage);
+    }
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.updateVisiblePages();
+      this.pageChange.emit(this.currentPage);
+    }
+  }
 }
