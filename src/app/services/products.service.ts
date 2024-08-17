7c163f3332a663455202a0ae3d4b1184909131da
omarknows <omarknows98@gmail.com>
2024-08-17 19:12:23 +0300

    removed redundant logic from products service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -18,44 +18,46 @@ export class ProductsService {
     category?: string | null,
     search?: string | null
   ) {
+    // Construct the URL based on the parameters
     let urlParts: string[] = [EApi.PRODUCTS];
 
+    // Add the search query and category to the URL if they exist
     if (search) {
       urlParts.push(`/search?q=${search}`);
     }
 
+    // Add the category to the URL if it exists
     if (category) {
       urlParts.push(`/category/${category}`);
     }
 
     const queryParams = new URLSearchParams();
 
+    // Add the pagination parameters to the URL if they exist
     if (params?.page && params.page > 1) {
       queryParams.set('skip', `${(params.page - 1) * 10}`);
     }
 
+    // Add the URL parameters to the URL
     const url = `${urlParts.join('')}${
       queryParams.toString() ? '?' + queryParams : ''
     }`;
 
-    return this.fetchAndCacheData<IResponse<IProduct, 'products'>>(url);
-  }
-
-  listCategories() {
-    const url = `${EApi.PRODUCTS}${EApi.CATEGORIES}`;
-    return this.fetchAndCacheData<ICategory[]>(url);
-  }
-
-  private fetchAndCacheData<T>(url: string) {
+    // Fetch the products from the API
     const cachedResponse = this.cachingService.get(url);
 
     if (cachedResponse) {
       return cachedResponse;
     } else {
-      return this.httpClient.get<T>(url).pipe(
+      return this.httpClient.get<IResponse<IProduct, 'products'>>(url).pipe(
         tap((response) => this.cachingService.set(url, response)),
         catchError((error: any) => throwError(() => new Error(error.message)))
       );
     }
+  }
+
+  // Fetch the categories from the API
+  listCategories() {
+    return this.httpClient.get<ICategory[]>(EApi.PRODUCTS + EApi.CATEGORIES);
   }
 }
