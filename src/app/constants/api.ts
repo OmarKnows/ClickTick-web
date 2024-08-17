ddf77fbef5315e3351fd9aee63474555c80cfe0d
omarknows <omarknows98@gmail.com>
2024-08-17 12:40:11 +0300

    created cart model and service

--- src/app/constants/api.ts
+++ src/app/constants/api.ts
@@ -1,9 +1,13 @@
 export enum EApi {
+  // auth related constants
   AUTH = 'auth',
   LOGIN = '/login',
-
+  // product related constants
   PRODUCTS = 'products',
   CATEGORIES = '/categories',
+  // cart related constants
+  CARTS = 'carts',
+  USER = '/user',
 }
 
 export const BASE_URL = 'https://dummyjson.com/';
